868513e752a579720a913083016a6651dceb1783
Vivek Choudhary <viveksirohi72@gmail.com>
2026-07-09 02:36:23 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& arr) {
+        int n=arr.size();
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j])
+                    return true;
+                }
+            }
+      return false;      
+    }
+};
